3a4b1fe2abc2c9c78d7b7aac1b79248bccde4632
rittikagarwal <rittik150@gmail.com>
2023-01-03 19:40:37 +0530

    first commit

---
+++ style.js
@@ -0,0 +1,35 @@
+let map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+  map.addListener('click', (event) => {
+    geocodeLatLng(event.latLng, map);
+  });
+}
+
+
+// Convert the latitude and longitude to an address using the Geocoding API
+  function geocodeLatLng(latLng, map) {
+    const geocoder = new google.maps.Geocoder;
+    geocoder.geocode({'location': latLng}, (results, status) => {
+      if (status === 'OK') {
+        if (results[0]) {
+          // Display the address, latitude, and longitude in the application
+          document.getElementById('address').innerText = results[0].formatted_address;
+          document.getElementById('lat').innerText = latLng.lat();
+          document.getElementById('lng').innerText = latLng.lng();
+        } else {
+          window.alert('No results found');
+        }
+      } else {
+        window.alert('Geocoder failed due to: ' + status);
+      }
+    });
+  }
+  
+  window.initMap = initMap;
+
+
